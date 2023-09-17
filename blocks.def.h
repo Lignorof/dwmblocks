e078f7f05a8afa6e5784a1afb6250715112bad8e
Lignorof <matheusepolis@outlook.com>
2023-09-17 17:51:20 -0300

    First commit.

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+
+	{"VOL: ", "pamixer --get-volume-human",					5,		0},
+
+
+	{"", "date '+%b %d %Y (%a) %T'",					5,		0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
